06900916fa0c50391a429716c18a21147afc4ed6
Rodrigo Felix <rodrigofelixferreira@hotmail.com>
2024-09-09 20:56:36 -0300

    conserta o cors

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from app.api.routes import router as api_router
 
 app = FastAPI()
 
 # Incluir o roteador
 app.include_router(api_router, prefix="/v1/api")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite todas as origens; ajuste conforme necessário
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos os métodos; ajuste conforme necessário
+    allow_headers=["*"],  # Permite todos os cabeçalhos; ajuste conforme necessário
+)
+
